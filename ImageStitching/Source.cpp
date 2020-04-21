e11fd0ed7b0fe511ec90b330f1bd0dad64a11d1b
Á¤½ģ <masa10269@gmail.com>
2020-04-22 00:52:54 +0800

    first empty project

---
+++ ImageStitching/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+//#include<opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "SIFT.h"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+	return 0;
+}
