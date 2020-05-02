bf962a880abeab8a6e00c73d8ed9adec44a76e87
KungYao <masa10269@gmail.com>
2020-05-02 16:35:42 +0800

    weight and gradient

---
+++ ImageStitching/FeatureUtil.h
@@ -0,0 +1,106 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+//#include<opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+template <typename T>
+class Vec2x {
+public:
+	T x = 0;
+	T y = 0;
+
+	friend ostream& operator<<(ostream& os, const Vec2x& vec) {
+		cout << "(x : " << vec.x << ", y : " << vec.y << ")";
+		return os;
+	}
+};
+
+#define Vec2i Vec2x<int>
+#define Vec2f Vec2x<float>
+
+class Feature {
+public:
+	// w(x, y) = exp(-(x^2 + y^2) / (2 * variance))
+	static std::vector<std::vector<float>> genWeightList(int window_size) {
+		std::vector<std::vector<float>> weights(0);
+		weights.reserve(window_size);
+		int ori_x, ori_y;
+		ori_x = ori_y = (window_size - 1) / 2;
+		float suqareSumOfWeight = 0;
+		for (int i = 0; i < window_size; i++) {
+			std::vector<float> tpmw(0);
+			tpmw.reserve(window_size);
+			int offset_y = i - ori_y;
+			for (int j = 0; j < window_size; j++) {
+				int offset_x = j - ori_x;
+				float diff_to_ori = offset_y * offset_y + offset_x * offset_x;
+				suqareSumOfWeight += diff_to_ori;
+				tpmw.push_back(diff_to_ori);
+			}
+			weights.push_back(tpmw);
+		}
+
+		int filterSize = window_size * window_size;
+		float mean = suqareSumOfWeight / filterSize;
+		float variance = suqareSumOfWeight / filterSize - mean * mean;
+		variance *= 2;
+
+		for (int i = 0; i < window_size; i++) {
+			for (int j = 0; j < window_size; j++) {
+				float *w = &weights[i][j];
+				*w = std::exp(*w / variance);
+			}
+		}
+
+		return weights;
+	}
+
+	// x和y方向
+	static std::vector<std::vector<Vec2f>> genImageGradient(const cv::Mat &img) {
+		cv::Mat grayImg;
+		img.copyTo(grayImg);
+		// cvtColor : src會被釋放?
+		cv::cvtColor(grayImg, grayImg, CV_BGR2GRAY);
+		std::vector<std::vector<Vec2f>> gradientMap(0);
+		gradientMap.reserve(grayImg.rows);
+		// rows : height
+		// cols : width
+		for (int i = 0; i < grayImg.rows; i++) {
+			std::vector<Vec2f> tmp;
+			tmp.reserve(grayImg.cols);
+			for (int j = 0; j < grayImg.cols; j++) {
+				Vec2f g;
+				// x direction
+				if (j - 1 >= 0)g.x -= grayImg.at<uchar>(i, j - 1);
+				if (j + 1 < grayImg.cols)g.x += grayImg.at<uchar>(i, j + 1);
+				// y direction
+				if (i - 1 >= 0)g.y -= grayImg.at<uchar>(i - 1, j);
+				if (i + 1 < grayImg.rows)g.y += grayImg.at<uchar>(i + 1, j);
+				tmp.push_back(g);
+			}
+			gradientMap.push_back(tmp);
+		}
+		return gradientMap;
+	}
+public:
+	static std::vector<std::vector<Vec2i>> HarrisCorner(const std::vector<cv::Mat> &images, int window_size) {
+		std::vector<std::vector<float>> weightList = genWeightList(window_size);
+		std::vector<std::vector<Vec2i>> featurePoints(0);
+		featurePoints.reserve(images.size());
+		for (const cv::Mat &img : images) {
+			std::vector<Vec2i> featureOfimg(0);
+			featureOfimg.reserve(128);
+
+			featureOfimg.resize(featureOfimg.size());
+			featurePoints.push_back(featureOfimg);
+		}
+		return featurePoints;
+	}
+};
--- ImageStitching/Source.cpp
+++ ImageStitching/Source.cpp
@@ -1,9 +1,71 @@
 ﻿#include <iostream>
-#include "HarrisCorner.h"
+#include "FeatureUtil.h"
+
+#define WINDOW_SIZE 9
+#define MyVec(x) Vec3b(x,x,x) 
 
 using namespace std;
 
-int main() {
+void test_weight_list();
+void test_gradient_map();
 
+int main() {
+	//test_weight_list();
+	//test_gradient_map();
+	system("pause");
 	return 0;
+}
+
+void test_weight_list() {
+	auto list = Feature::genWeightList(WINDOW_SIZE);
+	for (auto l1 : list) {
+		for (auto l2 : l1) {
+			cout << l2 << " ";
+		}
+		cout << "\n";
+	}
+	/*	
+		0.907286 0.926804 0.941001 0.949624 0.952516 0.949624 0.941001 0.926804 0.907286
+		0.926804 0.946741 0.961244 0.970052 0.973006 0.970052 0.961244 0.946741 0.926804
+		0.941001 0.961244 0.975969 0.984912 0.987912 0.984912 0.975969 0.961244 0.941001
+		0.949624 0.970052 0.984912 0.993937 0.996964 0.993937 0.984912 0.970052 0.949624
+		0.952516 0.973006 0.987912 0.996964 1 0.996964 0.987912 0.973006 0.952516
+		0.949624 0.970052 0.984912 0.993937 0.996964 0.993937 0.984912 0.970052 0.949624
+		0.941001 0.961244 0.975969 0.984912 0.987912 0.984912 0.975969 0.961244 0.941001
+		0.926804 0.946741 0.961244 0.970052 0.973006 0.970052 0.961244 0.946741 0.926804
+		0.907286 0.926804 0.941001 0.949624 0.952516 0.949624 0.941001 0.926804 0.907286
+	*/
+}
+
+void test_gradient_map() {
+	Mat fakeImage(3, 5, CV_8UC3);
+	fakeImage.at<Vec3b>(0, 0) = MyVec(0);
+	fakeImage.at<Vec3b>(0, 1) = MyVec(20);
+	fakeImage.at<Vec3b>(0, 2) = MyVec(50);
+	fakeImage.at<Vec3b>(0, 3) = MyVec(10);
+	fakeImage.at<Vec3b>(0, 4) = MyVec(0);
+	fakeImage.at<Vec3b>(1, 0) = MyVec(30);
+	fakeImage.at<Vec3b>(1, 1) = MyVec(200);
+	fakeImage.at<Vec3b>(1, 2) = MyVec(70);
+	fakeImage.at<Vec3b>(1, 3) = MyVec(45);
+	fakeImage.at<Vec3b>(1, 4) = MyVec(10);
+	fakeImage.at<Vec3b>(2, 0) = MyVec(7);
+	fakeImage.at<Vec3b>(2, 1) = MyVec(15);
+	fakeImage.at<Vec3b>(2, 2) = MyVec(55);
+	fakeImage.at<Vec3b>(2, 3) = MyVec(30);
+	fakeImage.at<Vec3b>(2, 4) = MyVec(0);
+	auto list2 = Feature::genImageGradient(fakeImage);
+	for (auto l1 : list2) {
+		for (auto l2 : l1) {
+			cout << l2 << " ";
+		}
+		cout << "\n";
+	}
+	/*
+		(x : 2, y : 3) (x : 6, y : 23) (x : -1, y : 8) (x : -6, y : 5) (x : -1, y : 1)
+		(x : 23, y : 29) (x : 5, y : 117) (x : -18, y : -4) (x : -7, y : 117) (x : -5, y : 134)
+		(x : 119, y : -3) (x : -27, y : 67) (x : -1, y : 23) (x : 132, y : 43) (x : -118, y : 119)
+		(x : 90, y : -27) (x : 31, y : -1) (x : -42, y : 132) (x : 89, y : -118) (x : -48, y : -44)
+		(x : 118, y : 0) (x : 132, y : -90) (x : -118, y : -31) (x : -44, y : -48) (x : 0, y : -120)
+	*/
 }
