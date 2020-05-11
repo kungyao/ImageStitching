e1f7ad7b38f8cdee64948449b3303cc1ab939466
KungYao <masa10269@gmail.com>
2020-05-11 17:56:10 +0800

    add feature descriptor and use opencv function instead.

--- ImageStitching/FeatureUtil.h
+++ ImageStitching/FeatureUtil.h
@@ -53,250 +53,142 @@ public:
 
 #define Vec2i Vec2x<int>
 #define Vec2f Vec2x<float>
-// odd
-#define WINDOW_SIZE 21
-#define GaussWeight std::vector<std::vector<float>>
 
-class Gaussian {
+using feature_pair = std::pair<float, Vec2i>;
+
+class FeatureInfo {
 public:
-	// w(x, y) = exp(-(x^2 + y^2) / (2 * variance))
-	static GaussWeight genGaussWeight(int window_size) {
-		GaussWeight weights(0);
-		weights.reserve(window_size);
-		int ori_x, ori_y;
-		ori_x = ori_y = (window_size - 1) / 2;
-		float sum = 0;
-		float sigma = 1;
-		float Q = 2 * sigma * sigma;
-		for (int i = 0; i < window_size; i++) {
-			std::vector<float> tpmw(0);
-			tpmw.reserve(window_size);
-			int offset_y = i - ori_y;
-			for (int j = 0; j < window_size; j++) {
-				int offset_x = j - ori_x;
-				float gauss_coeff = std::exp(-(offset_y * offset_y + offset_x * offset_x) / Q) / (Q * 3.1415926);
-				sum += gauss_coeff;
-				tpmw.push_back(gauss_coeff);
-			}
-			weights.push_back(tpmw);
-		}
-
-		// normalize
-		for (int i = 0; i < window_size; i++) {
-			for (int j = 0; j < window_size; j++) {
-				weights[i][j] /= sum;
-				//printf("%f ", weights[i][j]);
-			}
-			//printf("\n");
-		}
-		return weights;
+	// feature position
+	std::vector<Vec2i> pos;
+	// description
+	vector<vector<int>> descs;
+	FeatureInfo() {
+		pos.clear();
+		descs.clear();
 	}
-
-	static std::vector<std::vector<float>> filter(const std::vector<std::vector<float>> &img, const GaussWeight &weights) {
-		int half_x = 0, half_y = 0;
-		half_x = half_y = (WINDOW_SIZE - 1) / 2;
-		std::vector<std::vector<float>> f(img.size());
-		for (int i = 0; i < img.size(); i++) {
-			f[i].reserve(img[i].size());
-			for (int j = 0; j < img[i].size(); j++) {
-				float sum = 0;
-				for (int x = -half_x, im_x = j - half_x; im_x <= j + half_x && im_x < img[i].size(); x++, im_x++) {
-					if (im_x < 0)continue;
-					for (int y = -half_y, im_y = i - half_y; im_y <= i + half_y && im_y < img.size(); y++, im_y++) {
-						if (im_y < 0)continue;
-						sum += img[im_y][im_x] * weights[half_y + y][half_x + x];
-					}
-				}
-				f[i].push_back(sum);
-			}
-		}
-		return f;
-	}
-
-	static std::vector<std::vector<Vec2f>> filter(const std::vector<std::vector<Vec2f>> &img, const GaussWeight &weights) {
-		int half_x = 0, half_y = 0;
-		half_x = half_y = (WINDOW_SIZE - 1) / 2;
-		std::vector<std::vector<Vec2f>> f(img.size());
-		for (int i = 0; i < img.size(); i++) {
-			f[i].reserve(img[i].size());
-			for (int j = 0; j < img[i].size(); j++) {
-				Vec2f sum;
-				for (int x = -half_x, im_x = j - half_x; im_x <= j + half_x && im_x < img[i].size(); x++, im_x++) {
-					if (im_x < 0)continue;
-					for (int y = -half_y, im_y = i - half_y; im_y <= i + half_y && im_y < img.size(); y++, im_y++) {
-						if (im_y < 0)continue;
-						sum += img[im_y][im_x] * weights[half_y + y][half_x + x];
-					}
-				}
-				//std::cout << sum << std::endl;
-				f[i].push_back(sum);
-			}
-		}
-		return f;
-	}
-
-	static cv::Mat filter(const cv::Mat &img, const GaussWeight &weights) {
-		cv::Mat blur(img);
-		int half_x = 0, half_y = 0;
-		half_x = half_y = (WINDOW_SIZE - 1) / 2;
-		for (int i = 0; i < img.rows; i++) {
-			for (int j = 0; j < img.cols; j++) {
-				float sum = 0;
-				for (int x = -half_x, im_x = j - half_x; im_x <= j + half_x && im_x < img.cols; x++, im_x++) {
-					if (im_x < 0)continue;
-					for (int y = -half_y, im_y = i - half_y; im_y <= i + half_y && im_y < img.rows; y++, im_y++) {
-						if (im_y < 0)continue;
-						sum += img.at<uchar>(im_y, im_x) * weights[half_y + y][half_x + x];
-					}
-				}
-				blur.at<uchar>(i, j) = sum;
-			}
-		}
-		return blur;
-	}
-
-};
-
-class GradientImage {
-public:
-	std::vector<std::vector<Vec2f>> gradient;
-
-	std::vector<std::vector<float>> grad_xx;
-	std::vector<std::vector<float>> grad_yy;
-	std::vector<std::vector<float>> grad_xy;
-
-	std::vector<std::vector<float>> gauss_grad_xx;
-	std::vector<std::vector<float>> gauss_grad_yy;
-	std::vector<std::vector<float>> gauss_grad_xy;
-
-	GradientImage(const cv::Mat &img, GaussWeight *weights = NULL) {
-		bool ifDeletePointer = false;
-		if (weights == NULL) {
-			ifDeletePointer = true;
-			weights = new GaussWeight(Gaussian::genGaussWeight(WINDOW_SIZE));
-		}
-		//for (auto l1 : *weights) {
-		//	for (auto l2 : l1) {
-		//		cout << l2 << " ";
-		//	}
-		//	cout << "\n";
-		//}
-		cv::Mat grayImg;
-		// cvtColor : src會被釋放?
-		cv::cvtColor(img, grayImg, CV_BGR2GRAY);
-#ifdef WRITE_MIDDLE_STEP_IMAGE
-		cv::imwrite("./Feature/gray_image.png", grayImg);
-#endif // WRITE_MIDDLE_STEP_IMAGE
-
-		grayImg = Gaussian::filter(grayImg, *weights);
-
-#ifdef WRITE_MIDDLE_STEP_IMAGE
-		cv::imwrite("./Feature/blur_gray_image.png", grayImg);
-#endif // WRITE_MIDDLE_STEP_IMAGE
-
-		gradient.reserve(grayImg.rows);
-		// rows : height
-		// cols : width
-		for (int i = 0; i < grayImg.rows; i++) {
-			std::vector<Vec2f> tmp;
-			tmp.reserve(grayImg.cols);
-			for (int j = 0; j < grayImg.cols; j++) {
-				Vec2f g;
-				//// x direction
-				//if (j - 1 >= 0)g.x -= grayImg.at<uchar>(i, j - 1);
-				//if (j + 1 < grayImg.cols)g.x += grayImg.at<uchar>(i, j + 1);
-				//// y direction
-				//if (i - 1 >= 0)g.y -= grayImg.at<uchar>(i - 1, j);
-				//if (i + 1 < grayImg.rows)g.y += grayImg.at<uchar>(i + 1, j);
-
-				// x direction
-				g.x -= grayImg.at<uchar>(i, j);
-				if (j + 1 < grayImg.cols)g.x += grayImg.at<uchar>(i, j + 1);
-				else { 
-					g.x += grayImg.at<uchar>(i, j - 1); 
-					g.x *= -1;
-				}
-				// y direction
-				g.y -= grayImg.at<uchar>(i, j);
-				if (i + 1 < grayImg.rows)g.y += grayImg.at<uchar>(i + 1, j);
-				else {
-					g.y += grayImg.at<uchar>(i - 1, j);
-					g.y *= -1;
-				}
-				tmp.push_back(g);
-			}
-			gradient.push_back(tmp);
-		}
-
-#ifdef WRITE_MIDDLE_STEP_IMAGE
-		// for output gradient
-		cv::Mat grad_x_img(grayImg.rows, grayImg.cols, CV_8U);
-		cv::Mat grad_y_img(grayImg.rows, grayImg.cols, CV_8U);
-		for (int i = 0; i < grayImg.rows; i++) {
-			for (int j = 0; j < grayImg.cols; j++) {
-				grad_x_img.at<uchar>(i, j) = std::abs(gradient[i][j].x);
-				grad_y_img.at<uchar>(i, j) = std::abs(gradient[i][j].y);
-			}
-		}
-		cv::imwrite("./Feature/grad_x_img.png", grad_x_img);
-		cv::imwrite("./Feature/grad_y_img.png", grad_y_img);
-#endif // WRITE_MIDDLE_STEP_IMAGE
-
-		int indexNow = 0;
-		grad_xx.resize(gradient.size());
-		grad_yy.resize(gradient.size());
-		grad_xy.resize(gradient.size());
-		for (const std::vector<Vec2f> &row : gradient) {
-			grad_xx[indexNow].reserve(row.size());
-			grad_yy[indexNow].reserve(row.size());
-			grad_xy[indexNow].reserve(row.size());
-			for (const Vec2f &grad : row) {
-				grad_xx[indexNow].push_back(grad.x*grad.x);
-				grad_yy[indexNow].push_back(grad.y*grad.y);
-				grad_xy[indexNow].push_back(grad.x*grad.y);
-				//printf("%f ,%f ,%f\n", grad.x*grad.x, grad.y*grad.y, grad.x*grad.y);
-			}
-			indexNow++;
-		}
-
-		gauss_grad_xx = Gaussian::filter(grad_xx, *weights);
-		gauss_grad_yy = Gaussian::filter(grad_yy, *weights);
-		gauss_grad_xy = Gaussian::filter(grad_xy, *weights);
-
-		if (ifDeletePointer)
-			delete weights;
+	FeatureInfo(const std::vector<feature_pair> &tmpFeature, const int &maxSizeOfFeature) {
+		pos.reserve(maxSizeOfFeature);
+		for (int i = 0; i < tmpFeature.size() && i < maxSizeOfFeature; i++)pos.push_back(tmpFeature[i].second);
+		pos.resize(pos.size());
 	}
 };
-
-#define FeatureList std::vector<std::vector<Vec2i>>
+#define FeatureList std::vector<FeatureInfo>
 
 class Feature {
-public:
-	static FeatureList HarrisCorner(const std::vector<cv::Mat> &images, float threshold = 10000) {
-		GaussWeight weightList = Gaussian::genGaussWeight(WINDOW_SIZE);
-		FeatureList featurePoints(0);
-		featurePoints.reserve(images.size());
-		int half_x = 0, half_y = 0;
-		half_x = half_y = (WINDOW_SIZE - 1) / 2;
-		float k = 0.05;
-		for (const cv::Mat &img : images) {
-			std::vector<Vec2i> featureOfimg(0);
-			featureOfimg.reserve(128);
-			GradientImage gImg(img, &weightList);
-			printf("rows : %d, cols : %d\n", img.rows, img.cols);
-			for (int i = half_y; i < img.rows - half_y; i+= 1) {
-				for (int j = half_x; j < img.cols - half_x; j += 1) {
-					float denominator = gImg.gauss_grad_xx[i][j] + gImg.gauss_grad_yy[i][j];
-					float R = gImg.gauss_grad_xx[i][j] * gImg.gauss_grad_yy[i][j] - gImg.gauss_grad_xy[i][j] * gImg.gauss_grad_xy[i][j] - k * denominator * denominator;
-					if (R > threshold) {
-						featureOfimg.push_back(Vec2i(i, j));
-						//printf("%f\n", R);
+private:
+	static int maxFeatureSize;
+	// sift
+	static void featureDesctiptor(FeatureInfo &fInfo, const cv::Mat &angle) {
+		const auto& pos = fInfo.pos;
+		auto& descs = fInfo.descs;
+		for (int i = 0; i < pos.size(); i++) {
+			int x = pos[i].x;
+			int y = pos[i].y;
+
+			vector<int> votes(128, 0);
+			int blockStart[4] = { -8, -4, 1, 5 };
+			for (int by = 0; by < 4; by++)
+			{
+				int y_ = y + blockStart[by];
+				for (int bx = 0; bx < 4; bx++)
+				{
+					int x_ = x + blockStart[bx];
+					for (int dy = 0; dy < 4; dy++)
+					{
+						for (int dx = 0; dx < 4; dx++)
+						{
+							int idx = 8 * (4 * by + bx) + floor(angle.at<float>(y_ + dy, x_ + dx));
+							votes[idx]++;
+						}
 					}
 				}
 			}
-			featureOfimg.resize(featureOfimg.size());
-			featurePoints.push_back(featureOfimg);
+			descs.push_back(votes);
+		}
+	}
+public:
+	static FeatureList HarrisCorner(const std::vector<cv::Mat> &images, float threshold = 10000) {
+		FeatureList featurePoints(0);
+		featurePoints.reserve(images.size());
+		float k = 0.05;
+		for (const cv::Mat &img : images) {
+			cv::Mat grayImg;
+			cv::cvtColor(img, grayImg, CV_BGR2GRAY);
+			grayImg.convertTo(grayImg, CV_32FC1);
+
+			Mat Ix, Iy;
+			Mat kernelX(1, 3, CV_32F);
+			kernelX.at<float>(0, 0) = -1.0f;
+			kernelX.at<float>(0, 1) = 0.0f;
+			kernelX.at<float>(0, 2) = 1.0f;
+			filter2D(grayImg, Ix, CV_32F, kernelX);
+
+			Mat kernelY(3, 1, CV_32F);
+			kernelY.at<float>(0, 0) = -1.0f;
+			kernelY.at<float>(1, 0) = 0.0f;
+			kernelY.at<float>(2, 0) = 1.0f;
+			filter2D(grayImg, Iy, CV_32F, kernelY);
+
+#ifdef WRITE_MIDDLE_STEP_IMAGE
+			cv::imwrite("./Feature/grad_x_img.png", Ix);
+			cv::imwrite("./Feature/grad_y_img.png", Iy);
+#endif // WRITE_MIDDLE_STEP_IMAGE
+
+			cv::Mat gaus_Ix2;
+			cv::Mat gaus_Iy2;
+			cv::Mat gaus_Ixy;
+			cv::GaussianBlur(Ix.mul(Ix), gaus_Ix2, cv::Size(5, 5), 3);
+			cv::GaussianBlur(Iy.mul(Iy), gaus_Iy2, cv::Size(5, 5), 3);
+			cv::GaussianBlur(Ix.mul(Iy), gaus_Ixy, cv::Size(5, 5), 3);
+
+			cv::Mat trace = gaus_Ix2 + gaus_Iy2;
+			cv::Mat R = (gaus_Ix2.mul(gaus_Iy2) - gaus_Ixy.mul(gaus_Ixy)) - k * trace.mul(trace);
+
+			int dx_step[8] = { 1, 1, 0, -1, -1, -1,  0,  1 };
+			int dy_step[8] = { 0, 1, 1,  1,  0, -1, -1, -1 };
+			auto isLocalMaximum = [&](const cv::Mat img_r, const int &x, const int &y) {
+				for (const int& dx : dx_step) {
+					int rx = x + dx;
+					if (rx < 0 || rx >= img_r.cols)
+						continue;
+					for (const int& dy : dy_step) {
+						int ry = y + dy;
+						if (ry < 0 || ry >= img_r.rows)
+							continue;
+						if (img_r.at<float>(ry, rx) > img_r.at<float>(y, x))
+							return false;
+					}
+				}
+				return true;
+			};
+
+			std::vector<feature_pair> tmpFeature;
+			//printf("rows : %d, cols : %d\n", img.rows, img.cols);
+			for (int i = 0; i < img.rows; i++) {
+				for (int j = 0; j < img.cols; j++) {
+					float r = R.at<float>(i, j);
+					if (r > threshold && isLocalMaximum(R, j, i)) {
+						tmpFeature.push_back(feature_pair(r, Vec2i(i, j)));
+					}
+				}
+			}
+
+			auto r_val_compare = [&](const feature_pair &f1, const feature_pair& f2) {
+				return f1.first > f2.first;
+			};
+			std::sort(tmpFeature.begin(), tmpFeature.end(), r_val_compare);
+			FeatureInfo fea(tmpFeature, maxFeatureSize);
+
+			cv::Mat angle(Ix.size(), CV_32F);
+			for (int i = 0; i < Ix.rows; i++)
+				for (int j = 0; j < Ix.cols; j++)
+					angle.at<float>(i, j) = cv::fastAtan2(Ix.at<float>(i, j), Iy.at<float>(i, j)) / 45.0f;
+			featureDesctiptor(fea, angle);
+
+			featurePoints.push_back(fea);
 		}
 		return featurePoints;
 	}
 };
+
+int Feature::maxFeatureSize = 500;
--- ImageStitching/Source.cpp
+++ ImageStitching/Source.cpp
@@ -3,12 +3,8 @@
 #include <string>
 #include "FeatureUtil.h"
 
-#define MyVec(x) Vec3b(x,x,x) 
-
 using namespace std;
 
-void test_weight_list();
-void test_gradient_map();
 void visual_feature(const std::vector<cv::Mat> &images, const FeatureList &featureList);
 
 int main() {
@@ -25,11 +21,11 @@ int main() {
 }
 
 void visual_feature(const std::vector<cv::Mat> &images, const FeatureList &featureList) {
-	int half_x = 0, half_y = 0;
+	int half_x = 0, half_y = 0, WINDOW_SIZE = 11;
 	half_x = half_y = (WINDOW_SIZE - 1) / 2;
 	for (int i = 0; i < images.size(); i++) {
 		const cv::Mat &img = images[i];
-		const auto &fPoint = featureList[i];
+		const auto &fPoint = featureList[i].pos;
 		cv::Mat tmpImg;
 		img.copyTo(tmpImg);
 		for (const auto &p : fPoint) {
@@ -39,58 +35,4 @@ void visual_feature(const std::vector<cv::Mat> &images, const FeatureList &featu
 		string tmpOut = "./Feature/" + to_string(i);
 		cv::imwrite(tmpOut + "_image.png", tmpImg);
 	}
-}
-
-void test_weight_list() {
-	auto list = Gaussian::genGaussWeight(WINDOW_SIZE);
-	for (auto l1 : list) {
-		for (auto l2 : l1) {
-			cout << l2 << " ";
-		}
-		cout << "\n";
-	}
-	/*	
-		0.907286 0.926804 0.941001 0.949624 0.952516 0.949624 0.941001 0.926804 0.907286
-		0.926804 0.946741 0.961244 0.970052 0.973006 0.970052 0.961244 0.946741 0.926804
-		0.941001 0.961244 0.975969 0.984912 0.987912 0.984912 0.975969 0.961244 0.941001
-		0.949624 0.970052 0.984912 0.993937 0.996964 0.993937 0.984912 0.970052 0.949624
-		0.952516 0.973006 0.987912 0.996964 1 0.996964 0.987912 0.973006 0.952516
-		0.949624 0.970052 0.984912 0.993937 0.996964 0.993937 0.984912 0.970052 0.949624
-		0.941001 0.961244 0.975969 0.984912 0.987912 0.984912 0.975969 0.961244 0.941001
-		0.926804 0.946741 0.961244 0.970052 0.973006 0.970052 0.961244 0.946741 0.926804
-		0.907286 0.926804 0.941001 0.949624 0.952516 0.949624 0.941001 0.926804 0.907286
-	*/
-}
-
-void test_gradient_map() {
-	Mat fakeImage(3, 5, CV_8UC3);
-	fakeImage.at<Vec3b>(0, 0) = MyVec(0);
-	fakeImage.at<Vec3b>(0, 1) = MyVec(20);
-	fakeImage.at<Vec3b>(0, 2) = MyVec(50);
-	fakeImage.at<Vec3b>(0, 3) = MyVec(10);
-	fakeImage.at<Vec3b>(0, 4) = MyVec(0);
-	fakeImage.at<Vec3b>(1, 0) = MyVec(30);
-	fakeImage.at<Vec3b>(1, 1) = MyVec(200);
-	fakeImage.at<Vec3b>(1, 2) = MyVec(70);
-	fakeImage.at<Vec3b>(1, 3) = MyVec(45);
-	fakeImage.at<Vec3b>(1, 4) = MyVec(10);
-	fakeImage.at<Vec3b>(2, 0) = MyVec(7);
-	fakeImage.at<Vec3b>(2, 1) = MyVec(15);
-	fakeImage.at<Vec3b>(2, 2) = MyVec(55);
-	fakeImage.at<Vec3b>(2, 3) = MyVec(30);
-	fakeImage.at<Vec3b>(2, 4) = MyVec(0);
-	auto list2 = GradientImage(fakeImage, NULL);
-	for (auto l1 : list2.gradient) {
-		for (auto l2 : l1) {
-			cout << l2 << " ";
-		}
-		cout << "\n";
-	}
-	/*
-		(x : 2, y : 3) (x : 6, y : 23) (x : -1, y : 8) (x : -6, y : 5) (x : -1, y : 1)
-		(x : 23, y : 29) (x : 5, y : 117) (x : -18, y : -4) (x : -7, y : 117) (x : -5, y : 134)
-		(x : 119, y : -3) (x : -27, y : 67) (x : -1, y : 23) (x : 132, y : 43) (x : -118, y : 119)
-		(x : 90, y : -27) (x : 31, y : -1) (x : -42, y : 132) (x : 89, y : -118) (x : -48, y : -44)
-		(x : 118, y : 0) (x : 132, y : -90) (x : -118, y : -31) (x : -44, y : -48) (x : 0, y : -120)
-	*/
 }
